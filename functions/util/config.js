e57147a2e60d64ba8d1ae01eee73525371fc7d0c
Cristian <crystyxn@yahoo.com>
2020-08-24 22:05:03 +0200

    organizing

---
+++ functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyBH2RHjUjuqu6lEyM-Sdbe5syH2B9pSZJo",
+    authDomain: "social-media-app-43b7f.firebaseapp.com",
+    databaseURL: "https://social-media-app-43b7f.firebaseio.com",
+    projectId: "social-media-app-43b7f",
+    storageBucket: "social-media-app-43b7f.appspot.com",
+    messagingSenderId: "205597909661",
+    appId: "1:205597909661:web:0384b0e8c3a788cb146a74",
+    measurementId: "G-G8S9FQ7KDR"
+};
